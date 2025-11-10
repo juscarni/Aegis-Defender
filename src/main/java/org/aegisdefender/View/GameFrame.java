f7397da1f02186357ae63d24bcf60fab3bdf0e5e
juscarni <juscarni@gmail.com>
2025-11-10 13:01:49 +0100

    delete modification

--- src/main/java/org/aegisdefender/View/GameFrame.java
+++ src/main/java/org/aegisdefender/View/GameFrame.java
@@ -10,6 +10,5 @@ public class GameFrame extends JFrame {
         this.pack();
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         this.setLocationRelativeTo(null);
-        System.out.println("this is name!");
     }
 }
